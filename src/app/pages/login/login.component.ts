cff7c0299cf8899c67da1645d3ad036a82c0faa4
TccDS25 <tccds25@gmail.com>
2025-04-28 07:53:12 -0300

    estilizando os componentes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { AngularMaterialModule } from '../../angular_material/angular-material/angular-material.module';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [AngularMaterialModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  hidePassword = true;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
 }
